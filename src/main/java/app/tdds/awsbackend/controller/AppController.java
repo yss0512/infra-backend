2120bfa0b6488d40cee52a9885072e3e8cf50409
yss0512 <dbtntkd456@naver.com>
2025-07-18 11:41:57 +0900

    feat: EC2 상태 확인 로직 추가

--- src/main/java/app/tdds/awsbackend/controller/AppController.java
+++ src/main/java/app/tdds/awsbackend/controller/AppController.java
@@ -1,5 +1,7 @@
 package app.tdds.awsbackend.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +13,8 @@ public class AppController {
     return "Hello, World";
   }
 
+  @GetMapping("/health")
+  public ResponseEntity<?> checkEC2Status() {
+    return new ResponseEntity<String>("success Health", HttpStatus.OK);
+  }
 }
